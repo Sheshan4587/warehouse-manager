81d3e6d67af8a724df43617a69d25429c51e70ba
Sheshan4587 <sheshanthisal3@gmail.com>
2026-08-21 01:19:29 +0530

    feat: add Supplier routes for CRUD operations

--- routes/web.php
+++ routes/web.php
@@ -3,10 +3,17 @@
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\SupplierController;
 
+//Dashboard route
 Route::get('/', function () {
     return Inertia::render('Dashboard/Index');
 });
 
+//Category routes
 Route::resource('categories', CategoryController::class)
+    ->only(['index', 'store', 'update', 'destroy']);
+
+//Supplier routes
+Route::resource('suppliers', SupplierController::class)
     ->only(['index', 'store', 'update', 'destroy']);
